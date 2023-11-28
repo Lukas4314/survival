c08e2d2561975cc1528826da58228764d7a8e299
Lukas <lukashjeronimus@icloud.com>
2023-11-28 08:46:06 +0100

    har forsøgt med mere blending ift nabo pixels

--- src/App.java
+++ src/App.java
@@ -10,9 +10,9 @@ public class App extends PApplet {
     int width = 600;
     int[][] board;
     float avg;
+    float avg2;
     int counter;
     int hav = 4;
-    int blend = 3;
 
     public void settings() {
         size(height, width);
@@ -25,35 +25,26 @@ public class App extends PApplet {
                 board[i][j] = 128 + (int) random(-10, 10);
             }
         }
-        for (int k = 0; k < 1; k++) {
+        for (int k = 0; k < 70; k++) {
             for (int i = 0; i < height; i++) {
                 for (int j = 0; j < width; j++) {
                     if (i > hav && j > hav && i < width - hav && j < height - hav) {
-                        avg = 0;
-                        for (int l = -hav; l <= hav; l++) {
-                            for (int f = -hav; f <= hav; f++) {
-                                avg = avg + board[i - l][j - f];
-                            }
-                        }
-                        avg = avg / (hav * hav);
-                        board[i][j] = (int) avg + (int) random(-10, 10);
-                        /*
-                         * board[i][j] = (board[i - 1][j] +
-                         * board[i + 1][j] +
-                         * board[i][j - 1] +
-                         * board[i][j + 1] +
-                         * board[i - 1][j - 1] +
-                         * board[i + 1][j + 1] +
-                         * board[i - 1][j + 1] +
-                         * board[i + 1][j - 1]) / 8
-                         * + (int) random(-10, 10);
-                         */
+                        // (i != 0 && j != 0 && i != 599 && j != 599) {
+                        board[i][j] = (board[i - 1][j] +
+                                board[i + 1][j] +
+                                board[i][j - 1] +
+                                board[i][j + 1] +
+                                board[i - 1][j - 1] +
+                                board[i + 1][j + 1] +
+                                board[i - 1][j + 1] +
+                                board[i + 1][j - 1]) / 8
+                                + (int) random(-5, 5);
                     }
                 }
             }
         }
+
         counter = 0;
-        avg = 0;
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 if (i > hav && j > hav && i < width - hav && j < height - hav) {
@@ -63,6 +54,35 @@ public class App extends PApplet {
             }
         }
         avg = avg / counter;
+        // lav det mere pixled
+        for (int k = 1; k < 10; k++) {
+            for (int i = 0; i <= height; i = i + (int) Math.pow(2, k)) {
+                for (int j = 0; j <= width; j = j + (int) Math.pow(2, k)) {
+                    // System.out.println(j);
+                    if (i + (int) Math.pow(2, k) >= height || j + (int) Math.pow(2, k) >= width) {
+                        System.out.println("for stor");
+                        System.out.print("i:  ");
+                        System.out.println(i);
+                        System.out.print("j:  ");
+
+                        System.out.println(j);
+                        System.out.print("tal:");
+
+                        System.out.println((int) Math.pow(2, k));
+
+                        break;
+                    }
+                    avg2 = (board[i][j] + board[i + (int) Math.pow(2, k)][j] + board[i][j + (int) Math.pow(2, k)]
+                            + board[i + (int) Math.pow(2, k)][j + (int) Math.pow(2, k)]) / 4;
+                    // System.out.println(avg2);
+                    board[i][j] = (int) avg2;
+                    board[i + 1][j] = (int) avg2;
+                    board[i][j + 1] = (int) avg2;
+                    board[i + 1][j + 1] = (int) avg2;
+                }
+            }
+        }
+        System.out.println("done");
     }
 
     public void draw() {
